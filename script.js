1037851a063aa5f38833835f3452917ea93e4aeb
rimi <a.fatimaarimi@gmail.com>
2023-07-17 13:05:56 -0400

    wrote out my thought process on how to write out the script

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 /**
- * Memory Card Game:
+ * Memory Card Game (version 1):
+ *
+ * Player will first get a practice round:
+ * practice = 2x2 -- flip cards before timer runs out
+ * screen will display an array of 2 by 2 cards that take up a certain amount of space onscreen
  * 
+ * * player is prompted to click start 
+ * * when player clicks the start button; timer will run
+ * 
+ * gamePlay: ( a while loop with nested if else loop possibly? )
+ * * player will flip a card and the card will stay front-facing
+ * * player will then flip another card and will stay front-facing
+ * * the program will checkWin;
+ * * if (cards-match === true) {
+ * * the cards will stay front-facing;
+ * * } else if (card-match === false) {
+ * * cards will shake and flip to back-facing after 5 seconds
+ * * } else {
+ * * }
+ * 
+ * timer: (tbd)
+ * * if (timeRunsOut === true) {
+ * * gameOver;
+ * * } else {
+ * * gamePlay continues;
+ * * }
+ *
+ * Player will have the option to pick game setting: easy, medium, and hard;
+ * based off the game setting they will have a different amount of time to complete it
+ * * easy = 6x6 (time tbd)
+ * * medium = 8x8 (time tbd)
+ * * hard = 10x10 (time tbd)
+ *
+ *
  */
+
+
