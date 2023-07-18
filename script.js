03c0a20d0d8ae486884c5a5f36796b0e5d2af504
rimi <a.fatimaarimi@gmail.com>
2023-07-18 15:15:27 -0400

    finally got the cards to flip

--- script.js
+++ script.js
@@ -26,7 +26,10 @@
  * * gamePlay continues;
  * * }
  *
- * Player will have the option to pick game setting: easy, medium, and hard;
+ * counter:
+ * count how many times memory card was click when back-facing
+ *
+ * Player will have the option to pick game setting: easy, medium, and hard;toggle
  * based off the game setting they will have a different amount of time to complete it
  * * easy = 6x6 (time tbd)
  * * medium = 8x8 (time tbd)
@@ -35,4 +38,56 @@
  *
  */
 
+/**
+ * What do I need to make this work?
+ *
+ * -> gameObject
+ * -> a flipCard function
+ * -> some sort of way to identify if the cards match
+ */
 
+// *********** timer function ***********
+const timer = document.querySelector("#timer");
+// add a when user clicks start button the timer is triggered
+
+// *********** moves counter function ***********
+// function createCounter() {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     return count;
+//   };
+// }
+// const counter = createCounter();
+// const countDisplay = document.querySelector("#counter");
+// countDisplay.addEventListener("click", () => {
+//   countDisplay.innerHTML = counter();
+// });
+
+// *********** card flip function ***********
+// selecting all the memory cards
+const memoryCards = document.querySelectorAll(".memory-card");
+
+//! converted the NodeList to an array because when I first added the forEach card event listener I kept getting a typeError saying the function was not a function -> this was because it was reading the memory cards as a nodeList and I cant do the forEach function for a nodeList but I can for an array 
+const cardsArray = [...memoryCards];
+
+// iterate over each memory card and add the click event listener
+cardsArray.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
+
+// lock board will lock board so multiple cards cant be flipped and I set it to false because at the beginning of the game no cards will flip until game has started
+let lockBoard = false;
+// going to store the first card thats flipped
+let firstCard;
+
+// created a call back function for flipCard to actually flip the cards now
+function flipCard() {
+    // Check if the board is locked or if the card is already flipped, or if the same card is clicked
+    if (lockBoard || this === firstCard || this.classList.contains("flip")) {
+      return;
+    }
+  
+    // Add the flip class to the clicked card so we know that it has already been flipped
+    this.classList.add("flip"); 
+}
